c3f1e0e2c9cdba22ad8dbb9bfddca37b4f0b655e
Varstasdp <d.pocius93@gmail.com>
2023-03-03 16:48:39 +0000

    Save nice fade in effect I made for my future projects

--- Layout_and_Transition_Practice/Scrolling_Fade_In_Test/app.js
+++ Layout_and_Transition_Practice/Scrolling_Fade_In_Test/app.js
@@ -1,12 +1,18 @@
 const observer = new IntersectionObserver((entries)=>{
-    entries.forEach((entry)=>{
-        console.log(entry)
-        if(entry.isIntersecting) {
-            entry.target.classList.add("show");
-        } else {
-            entry.target.classList.remove("show");
-        }
-    });
-});
+        entries.forEach((entry)=>{
+            console.log(entry)
+            if(entry.isIntersecting) {
+                entry.target.classList.add("show");
+                observer.unobserve(entry.target)
+            } 
+            // else {
+            //     entry.target.classList.remove("show");
+            // }
+        });
+    },
+    {
+        threshold: .50,
+    }
+);
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
